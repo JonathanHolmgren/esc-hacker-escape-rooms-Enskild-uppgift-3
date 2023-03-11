5dc023b7a66e2eefa8f51848c6ad36155ecb053e
jonathanholmgren <jonathan.holmgren@hotmail.com>
2023-03-11 14:34:29 +0100

    La till att cypress test ska kolla h1

--- cypress/e2e/sample_spec.cy.js
+++ cypress/e2e/sample_spec.cy.js
@@ -1,5 +1,6 @@
 describe('This test is to check if my server working', () => {
     it('Visits the localhost of my site', () => {
         cy.visit('http://localhost:3080')
+        cy.get('h1').should('contain', 'Hacker Escape Rooms')
     })
 }) 
